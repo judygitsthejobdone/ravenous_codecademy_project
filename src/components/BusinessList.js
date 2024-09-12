932dae500f1ecf07c9f12c1bb74f20f8ba916182
Judah Mendez <judysubscribes@gmail.com>
2024-09-12 12:00:39 -0400

    moves hardcoded business to list component and allows for business input to be an object. Cleans BusinessList code to use array of li's and removes unused code

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,16 +1,34 @@
-//import './BusinessList.css';
-import Business from "./Business"
+import Business from "./Business";
+
+const hardcodedBusiness = {
+    name: "Uncle Chip's",
+    address: "1100 North Capitol St",
+    city: "Washington",
+    state: "DC",
+    zipcode: "20001",
+    category: "American",
+    rating: "4.8",
+    review_count: "237",
+    src: "https://lh3.googleusercontent.com/p/AF1QipOQI5WU8mTmYDxDXuySRrOlI05XGWM42wnk6X0B=s1360-w1360-h1020",
+    alt: "Uncle Chip's businesss photo"
+}; //DEBUG code, used for testing
+
+let businessArray = [
+    <Business business={hardcodedBusiness}/>,
+    <Business business={hardcodedBusiness}/>,
+    <Business business={hardcodedBusiness}/>,
+    <Business business={hardcodedBusiness}/>,
+    <Business business={hardcodedBusiness}/>,
+    <Business business={hardcodedBusiness}/>
+];
 
 function BusinessList() {
-  return (
+    const list = businessArray.map(business=><li>{business}</li>);
+
+    return (
     <div className="BusinessList">
       <ul>
-        <li><Business /></li>
-        <li><Business /></li>
-        <li><Business /></li>
-        <li><Business /></li>
-        <li><Business /></li>
-        <li><Business /></li>
+        {list}
       </ul>
     </div>
   );
