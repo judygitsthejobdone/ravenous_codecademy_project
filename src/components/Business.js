62d9b9d2ac8d5c4afc22a7585721d390f0d1df8f
Judah Mendez <judysubscribes@gmail.com>
2024-09-16 16:06:56 -0400

    adds styling to Business component

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,17 +1,21 @@
+import './Business.css'
 function Business(props) {
   return (
     <div className="business">
         <img src={props.business.src} alt={props.business.alt} />
         <p className="business-name">{props.business.name}</p>
-        <p className="business-address">
-            {props.business.address}<br />
-            {props.business.city}<br />
-            {props.business.state}<br />
-            {props.business.zipcode}
-        </p>
-        <p className="business-category">{props.business.category}</p>
-        <p className="business-rating">{props.business.rating}</p>
-        <p className="business-review-count">{props.business.review_count}</p>
+        <div className="business-info">
+            <p className="address">
+                {props.business.address}<br />
+                {props.business.city}<br />
+                {props.business.state} {props.business.zipcode}
+            </p>
+            <div className="column2">
+                <p className="business-category">{props.business.category}</p>
+                <p className="business-rating">{props.business.rating} stars</p>
+                <p className="business-review-count">{props.business.review_count} reviews</p>
+            </div>
+        </div>
     </div>
   );
 }
