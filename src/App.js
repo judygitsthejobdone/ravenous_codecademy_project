59f9f8dc2e67647d5a69889115ca29c1e451ca75
Judah Mendez <judysubscribes@gmail.com>
2024-09-17 16:05:30 -0400

    adds mock data to api directory, imports it to App, and passes it down to BusinessList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import BusinessList from './components/BusinessList';
 import SearchBar from './components/SearchBar';
+import data from './api/mock'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         </p>
       </header>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businessArray={data}/>
     </div>
   );
 }
