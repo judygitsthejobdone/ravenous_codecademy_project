8b14b291530178f222aa84cd3825a9a02417c56d
Judah Mendez <judysubscribes@gmail.com>
2024-09-17 16:19:58 -0400

    cleans up code in js and css

--- src/components/Business.js
+++ src/components/Business.js
@@ -3,19 +3,23 @@ function Business(props) {
   return (
     <div className="business">
         <img src={props.business.src} alt={props.business.alt} />
-        <p className="business-name">{props.business.name}</p>
+        <h2>{props.business.name}</h2>
+        
         <div className="business-info">
             <p className="address">
                 {props.business.address}<br />
                 {props.business.city}<br />
                 {props.business.state} {props.business.zipcode}
             </p>
+            
             <div className="column2">
-                <p className="business-category">{props.business.category}</p>
-                <p className="business-rating">{props.business.rating} stars</p>
+                <h3>{props.business.category}</h3>
+                <h4>{props.business.rating} stars</h4>
                 <p className="business-review-count">{props.business.review_count} reviews</p>
             </div>
+        
         </div>
+    
     </div>
   );
 }
