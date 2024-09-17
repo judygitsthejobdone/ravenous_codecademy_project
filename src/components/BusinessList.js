7733ae775228ca547baa04e6713a58b77b99ea13
Judah Mendez <judysubscribes@gmail.com>
2024-09-17 16:12:30 -0400

    refactors to use props from App and passes individual business to the Business component

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,35 +1,11 @@
 import Business from "./Business";
 import './BusinessList.css'
 
-const hardcodedBusiness = {
-    name: "Uncle Chip's",
-    address: "1100 North Capitol St",
-    city: "Washington",
-    state: "DC",
-    zipcode: "20001",
-    category: "American",
-    rating: "4.8",
-    review_count: "237",
-    src: "https://lh3.googleusercontent.com/p/AF1QipOQI5WU8mTmYDxDXuySRrOlI05XGWM42wnk6X0B=s1360-w1360-h1020",
-    alt: "Uncle Chip's businesss photo"
-}; //DEBUG code, used for testing
-
-let businessArray = [
-    <Business business={hardcodedBusiness}/>,
-    <Business business={hardcodedBusiness}/>,
-    <Business business={hardcodedBusiness}/>,
-    <Business business={hardcodedBusiness}/>,
-    <Business business={hardcodedBusiness}/>,
-    <Business business={hardcodedBusiness}/>
-];
-
-function BusinessList() {
-    const list = businessArray.map(business=><li>{business}</li>);
-
+function BusinessList(props) {
     return (
     <div className="Business-List">
       <ul>
-        {list}
+        {props.businessArray.map(element=><li><Business business={element}/></li>)}
       </ul>
     </div>
   );
