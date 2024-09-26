267e74509727b1881fa17bda9ba6ac75ce458241
Judah Mendez <judysubscribes@gmail.com>
2024-09-26 16:13:06 -0400

    imports chakra to BusinessList and applies basic styling

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,12 +1,14 @@
 import Business from "./Business";
-import './BusinessList.css'
+import './BusinessList.css';
+import { Box, Flex, Spacer } from '@chakra-ui/react';
+
 
 function BusinessList(props) {
     return (
     <div className="Business-List">
-      <ul>
-        {props.businessArray.map(element=><li><Business business={element}/></li>)}
-      </ul>
+      <Flex justifyContent="space-between" flexWrap="wrap" maxWidth="100%" >
+        {props.businessArray.map(element=><Box className="business" width="30%"><Business business={element}/></Box>)}
+      </Flex>
     </div>
   );
 }
