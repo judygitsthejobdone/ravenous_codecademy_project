e73529c3d53fab9f305fc056dc6f8f9b3fc1e7ec
Judah Mendez <judysubscribes@gmail.com>
2024-09-27 19:06:22 -0400

    installs whatwg-fetch polyfill for compatibility, creates API file structure, and adds apiKey

---
+++ src/utils/YelpAPI.js
@@ -0,0 +1,3 @@
+const ClientID = 'fkKEWOBAhThb7YBikSbwAg';
+const APIKey = 'dsAACaZfAgL0c6k-mm_eRxcR0N820U-Lbmmten64zWrFwzB2nqtop2thYKQ7YU-X1I9MfFlK39Hu9cfaruGsYNUXerUX252q7aCiH9F7vCdatoGEY1QNnMKQjTj3ZnYx';
+
