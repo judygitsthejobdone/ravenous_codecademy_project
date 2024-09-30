b3c3fad8a504a628c7b00f6d94920a4ecf9e6a1c
Judah Mendez <judysubscribes@gmail.com>
2024-09-30 16:26:54 -0400

    adds unique key to each box parenting a business component

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -7,7 +7,7 @@ function BusinessList(props) {
     return (
     <div>
       <Flex className="Business-List" justifyContent='space-between' flexFlow='wrap' maxWidth="100%">
-        {props.businessArray.map(element=><Box className="business"><Business business={element}/></Box>)}
+        {props.businessArray.map((element, index)=><Box className="business" key={index}><Business business={element}/></Box>)}
       </Flex>
     </div>
   );
